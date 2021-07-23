dc155f335633822399db7bd94f03a2094f0815da
Shreekz <alanmguadet@gmail.com>
2021-07-23 11:29:03 -0400

    added mutations to resolvers and typedefs on server side

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,12 +2,14 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
-    _id: ID
+    _id: ID!
     username: String
     email: String
     password: String
   }
-type Contact {
+
+type Resume {
+  _id: ID!
   name: String
   city: String
   state: String
@@ -18,14 +20,37 @@ type Contact {
   skills: [String]
 }
 
-type: Projects {
+input Resume {
+  _id: ID!
+  name: String
+  city: String
+  state: String
+  address: String
+  zip: String
+  phoneNumber: String
+  summary: String
+  skills: [String]
+  projects: [Project]
+  education: [Education]
+  experience: [Experience]
+}
+
+type Project {
   title: String
   link: String
   bPoints: [String]
   tools: [String]
 }
 
-type: Experience {
+input ProjectInput {
+  title: String
+  link: String
+  bPoints: [String]
+  tools: [String]
+}
+
+
+type Experience {
   title: String
   role: String
   date: String
@@ -34,7 +59,23 @@ type: Experience {
   keyAchievement: String
 }
 
-type: Education {
+input ExperienceInput {
+  title: String
+  role: String
+  date: String
+  location: String
+  description: String
+  keyAchievement: String
+}
+
+type Education {
+  title: String
+  date: String
+  bPoints: [String]
+  description: String
+}
+
+input EducationInput {
   title: String
   date: String
   bPoints: [String]
@@ -55,6 +96,14 @@ type: Education {
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
+    createResume(name: String!, city: String!, state: String!, address: String!, zip: String!, phoneNumber: String!, summary: String!)
+    addResume(resumeData: $ResumeInput): User
+    createExp(title: String!, role: String!, date: String!, location: String!, description: String!, keyAchievement: String!)
+    createEd(title: String!, date: String!, bPoints: [String], description: String!)
+    createProject(title: String!, link: String!, bPoints: [String], tools: [String])
+    addExp(expData: $ExperienceInput): Resume
+    addEducation(educationData: $EducationInput): Resume
+    addProject(projectData: $projectInput): Resume
   }
 `;
 
