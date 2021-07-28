3d61244c883df98af85164aac0342e279c625b73
Ryan <ryanpaynt@uncc.edu>
2021-07-28 10:50:04 -0400

    fixed pdf

--- client/src/pages/Resume.js
+++ client/src/pages/Resume.js
@@ -1,7 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useQuery } from '@apollo/client';
 import { Link } from 'react-router-dom';
 import Auth from '../utils/auth';
+import jsPDF from 'jspdf';
+import pdf from 'html-pdf';
+import html2canvas from 'html2canvas';
 
 import { QUERY_USERS, QUERY_USER, QUERY_ME } from '../utils/queries';
 // Components
@@ -9,6 +12,19 @@ import UserList from '../components/UserList';
 import './profile.css'
 
 const Resume = () => {
+
+  const mainEl = React.useRef(null);
+
+  function createPDF(e) {
+    const tempHTML = document.querySelector('#inner');
+    const bounds = tempHTML.getBoundingClientRect();
+    window.scrollTo(0, bounds.y);
+    html2canvas(tempHTML).then(canvas => {
+      console.log(canvas);
+      const image = canvas.toDataURL('image/jpeg');
+      e.target.href = image;
+    });
+  }
 
   useEffect(() => {
     document.title = `Rapid Résumé | Profile`;
@@ -52,162 +68,143 @@ const Resume = () => {
       </head>
 
       <body>
-        <div id="doc2" className="yui-t7">
-          <div id="inner">
+        <main ref={mainEl}>
+          <div id="doc2" className="yui-t7">
+            <div id="inner">
 
-            <div id="hd">
-              <div className="yui-gc">
-                <div className="yui-u first">
-                  <h1>Jonathan Doe</h1>
-                  <h2>Web Designer, Director</h2>
-                </div>
-
-                <div className="yui-u">
-                  <div className="contact-info">
-
-                    <h3><a href="mailto:name@yourdomain.com">name@yourdomain.com</a></h3>
-                    <h3>(313) - 867-5309</h3>
+              <div id="hd">
+                <div className="yui-gc">
+                  <div className="yui-u first">
+                    <h1>Jonathan Doe</h1>
+                    <h2>Web Designer, Director</h2>
                   </div>
-                  {/* <!--// .contact-info --> */}
+
+                  <div className="yui-u">
+                    <div className="contact-info">
+
+                      <h3><a href="mailto:name@yourdomain.com">name@yourdomain.com</a></h3>
+                      <h3>(313) - 867-5309</h3>
+                    </div>
+                    {/* <!--// .contact-info --> */}
+                  </div>
+                </div>
+                {/* <!--// .yui-gc --> */}
+              </div>
+              {/* <!--// hd --> */}
+
+              <div id="bd">
+                <div id="yui-main">
+                  <div className="yui-b">
+
+                    <div className="yui-gf">
+                      <div className="yui-u first">
+                        <h2>Profile</h2>
+                      </div>
+                      <div className="yui-u">
+                        <p className="enlarge">
+                          Progressively evolve cross-platform ideas before impactful infomediaries. Energistically visualize
+                          tactical initiatives before cross-media catalysts for change.
+                        </p>
+                      </div>
+                    </div>
+                    {/* <!--// .yui-gf --> */}
+
+                    <div className="yui-gf">
+                      <div className="yui-u first">
+                        <h2>Skills</h2>
+                      </div>
+                      <div className="yui-u">
+
+                        <div className="talent">
+                          <h2>Web Design</h2>
+                          <p>Assertively exploit wireless initiatives rather than synergistic core competencies. </p>
+                        </div>
+
+                        <div className="talent">
+                          <h2>Interface Design</h2>
+                          <p>Credibly streamline mission-critical value with multifunctional functionalities. </p>
+                        </div>
+
+                        <div className="talent">
+                          <h2>Project Direction</h2>
+                          <p>Proven ability to lead and manage a wide variety of design and development projects in team and
+                            independent situations.</p>
+                        </div>
+                      </div>
+                    </div>
+                    {/* <!--// .yui-gf --> */}
+
+                    <div className="yui-gf">
+                      <div className="yui-u first">
+                        <h2>Technical</h2>
+                      </div>
+                      <div className="yui-u">
+                        <ul className="talent">
+                          <li>XHTML</li>
+                          <li>CSS</li>
+                          <li className="last">Javascript</li>
+                        </ul>
+
+                        <ul className="talent">
+                          <li>Jquery</li>
+                          <li>PHP</li>
+                          <li className="last">CVS / Subversion</li>
+                        </ul>
+
+                        <ul className="talent">
+                          <li>OS X</li>
+                          <li>Windows XP/Vista</li>
+                          <li className="last">Linux</li>
+                        </ul>
+                      </div>
+                    </div>
+                    {/* <!--// .yui-gf--> */}
+
+                    <div className="yui-gf">
+
+                      <div className="yui-u first">
+                        <h2>Experience</h2>
+                      </div>
+                      {/* <!--// .yui-u --> */}
+                      <div className="yui-u">
+                        <div className="job last">
+                          <h2>International Business Machines (IBM)</h2>
+                          <h3>Lead Web Designer</h3>
+                          <h4>2001-2004</h4>
+                          <p>Globally re-engineer cross-media schemas through viral methods of empowerment. Proactively grow
+                            long-term high-impact human capital and highly efficient innovation. Intrinsicly iterate excellent
+                            e-tailers with timely e-markets.</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="yui-gf last">
+                      <div className="yui-u first">
+                        <h2>Education</h2>
+                      </div>
+                      <div className="yui-u">
+                        <h2>Indiana University - Bloomington, Indiana</h2>
+                        <h3>Dual Major, Economics and English &mdash; <strong>4.0 GPA</strong> </h3>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
-              {/* <!--// .yui-gc --> */}
-            </div>
-            {/* <!--// hd --> */}
 
-            <div id="bd">
-              <div id="yui-main">
-                <div className="yui-b">
-
-                  <div className="yui-gf">
-                    <div className="yui-u first">
-                      <h2>Profile</h2>
-                    </div>
-                    <div className="yui-u">
-                      <p className="enlarge">
-                        Progressively evolve cross-platform ideas before impactful infomediaries. Energistically visualize
-                        tactical initiatives before cross-media catalysts for change.
-                      </p>
-                    </div>
-                  </div>
-                  {/* <!--// .yui-gf --> */}
-
-                  <div className="yui-gf">
-                    <div className="yui-u first">
-                      <h2>Skills</h2>
-                    </div>
-                    <div className="yui-u">
-
-                      <div className="talent">
-                        <h2>Web Design</h2>
-                        <p>Assertively exploit wireless initiatives rather than synergistic core competencies. </p>
-                      </div>
-
-                      <div className="talent">
-                        <h2>Interface Design</h2>
-                        <p>Credibly streamline mission-critical value with multifunctional functionalities. </p>
-                      </div>
-
-                      <div className="talent">
-                        <h2>Project Direction</h2>
-                        <p>Proven ability to lead and manage a wide variety of design and development projects in team and
-                          independent situations.</p>
-                      </div>
-                    </div>
-                  </div>
-                  {/* <!--// .yui-gf --> */}
-
-                  <div className="yui-gf">
-                    <div className="yui-u first">
-                      <h2>Technical</h2>
-                    </div>
-                    <div className="yui-u">
-                      <ul className="talent">
-                        <li>XHTML</li>
-                        <li>CSS</li>
-                        <li className="last">Javascript</li>
-                      </ul>
-
-                      <ul className="talent">
-                        <li>Jquery</li>
-                        <li>PHP</li>
-                        <li className="last">CVS / Subversion</li>
-                      </ul>
-
-                      <ul className="talent">
-                        <li>OS X</li>
-                        <li>Windows XP/Vista</li>
-                        <li className="last">Linux</li>
-                      </ul>
-                    </div>
-                  </div>
-                  {/* <!--// .yui-gf--> */}
-
-                  <div className="yui-gf">
-
-                    <div className="yui-u first">
-                      <h2>Experience</h2>
-                    </div>
-                    {/* <!--// .yui-u --> */}
-                    <div className="yui-u">
-                      <div className="job last">
-                        <h2>International Business Machines (IBM)</h2>
-                        <h3>Lead Web Designer</h3>
-                        <h4>2001-2004</h4>
-                        <p>Globally re-engineer cross-media schemas through viral methods of empowerment. Proactively grow
-                          long-term high-impact human capital and highly efficient innovation. Intrinsicly iterate excellent
-                          e-tailers with timely e-markets.</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="yui-gf last">
-                    <div className="yui-u first">
-                      <h2>Education</h2>
-                    </div>
-                    <div className="yui-u">
-                      <h2>Indiana University - Bloomington, Indiana</h2>
-                      <h3>Dual Major, Economics and English &mdash; <strong>4.0 GPA</strong> </h3>
-                    </div>
-                  </div>
-                </div>
+              <div id="ft">
+                <p>Jonathan Doe &mdash; <a href="mailto:name@yourdomain.com">name@yourdomain.com</a> &mdash; (313) - 867-5309
+                </p>
               </div>
-            </div>
+              {/* <!--// footer --> */}
 
-            <div id="ft">
-              <p>Jonathan Doe &mdash; <a href="mailto:name@yourdomain.com">name@yourdomain.com</a> &mdash; (313) - 867-5309
-              </p>
             </div>
-            {/* <!--// footer --> */}
+            {/* <!-- // inner --> */}
 
           </div>
-          {/* <!-- // inner --> */}
-
-        </div>
-        {/* <!--// doc --> */}
-
-
+          <a download='resume.jpeg' href='.' onClick={createPDF}>click</a>
+          {/* <!--// doc --> */}
+        </main>
       </body>
-      {/* <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.0.272/jspdf.debug.js"></script>
-<script>
-	const main = document.querySelector("main");
-	const button = document.querySelector("button");
-
-	function createPDF() {
-		var pdf = new jsPDF('p', 'pt', 'a4');
-		pdf.addHTML(main, function () {
-			pdf.save('web.pdf');
-		});
-	}
-
-	button.addEventListener("click", () => {
-		button.style.display = 'none';
-		createPDF();
-	});
-</script> */}
-
     </html>
   );
 };
-
 export default Resume;
